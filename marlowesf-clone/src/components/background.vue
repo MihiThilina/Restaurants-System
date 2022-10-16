eab80e7a6e9e3b40bcb76ed7b92b469afefe9497
MihiThilina <thilinabandar67@gmail.com>
2022-10-17 00:40:51 +0530

    updated background

---
+++ marlowesf-clone/src/components/background.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+
+
+</script>
+
+<template>
+
+    <main>
+    
+        <va-image
+                class="background-img"
+                src="../../src/assets/img/Background-subwaytile.jpeg"
+                style="height: 17vh"
+              />
+    </main>
+
+</template>
+
+<style>
+    
+</style>
--- marlowesf-clone/src/main.js
+++ marlowesf-clone/src/main.js
@@ -1,6 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createVuestic } from 'vuestic-ui'
+import 'vuestic-ui/css'
+import '../src/style/backgrond.css'
 
-import './assets/main.css'
 
-createApp(App).mount('#app')
+
+const app = createApp(App)
+app.use(createVuestic())
+app.mount('#app')
